5e77613a3b38fc5d93cdc5d6929f409b9498c1bb
Francis Bulu <Francis.Bulu@student.intecbrussel.be>
2021-05-25 12:04:34 +0200

    Add project files.

---
+++ SessionObject/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SessionObject.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace SessionObject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var userInfo = new UserInfo()
+            {
+                Id = 1,
+                Name = "Franck"
+            };
+            HttpContext.Session.SetString("SessionUser", JsonConvert.SerializeObject(userInfo));
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
---
+++ SessionObject/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SessionObject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SessionObject.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            var user = JsonConvert.DeserializeObject<UserInfo>(HttpContext.Session.GetString("SessionUser"));
+            user.Id = 50;
+            user.Name = "Stylz";
+            return View(user);
+        }
+    }
+}
